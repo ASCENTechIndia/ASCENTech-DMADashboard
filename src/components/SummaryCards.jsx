d5c678a08117f24e53a5c0354e828e413fc652dc
Swati-Ascentech <swati.gupta@ascentechindia.com>
2026-06-26 11:12:05 +0530

    d

--- src/components/SummaryCards.jsx
+++ src/components/SummaryCards.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import React, { memo } from "react";
-import { summaryData } from "../data/dashboardData";
 import { FaFileAlt, FaRupeeSign, FaWallet, FaChartPie } from "react-icons/fa";
 import axios from "axios";
 
